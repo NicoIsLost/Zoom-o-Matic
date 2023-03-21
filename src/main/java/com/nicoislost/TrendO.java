37697886f3bc2d65a8fedd4d4884f75ca0460f09
NicoIsLost <contact@nicoislost.com>
2023-03-21 22:13:57 +0100

    Added keybindings

--- src/main/java/com/nicoislost/TrendO.java
+++ src/main/java/com/nicoislost/TrendO.java
@@ -1,12 +1,35 @@
 package com.nicoislost;
 
 import net.fabricmc.api.ClientModInitializer;
-
+import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.minecraft.client.option.KeyBinding;
+import net.minecraft.client.util.InputUtil;
+import org.lwjgl.glfw.GLFW;
 
 public class TrendO implements ClientModInitializer {
 
+	private static KeyBinding keyBinding1;
+	private static KeyBinding keyBinding2;
+	private static KeyBinding keyBinding3;
+
 	@Override
 	public void onInitializeClient() {
+		keyBinding1 = KeyBindingHelper.registerKeyBinding(new KeyBinding(
+				"key.nicoislost.zoom1",
+				InputUtil.Type.KEYSYM,
+				GLFW.GLFW_KEY_C,"category.nicoislost.zoom"
+		));
 
+		keyBinding1 = KeyBindingHelper.registerKeyBinding(new KeyBinding(
+				"key.nicoislost.zoom2",
+				InputUtil.Type.KEYSYM,
+				GLFW.GLFW_KEY_UNKNOWN,"category.nicoislost.zoom"
+		));
+
+		keyBinding1 = KeyBindingHelper.registerKeyBinding(new KeyBinding(
+				"key.nicoislost.zoom3",
+				InputUtil.Type.KEYSYM,
+				GLFW.GLFW_KEY_UNKNOWN,"category.nicoislost.zoom"
+		));
 	}
 }
