fa2a712f2aa2e1095508fd42af70c1cf36c7fd8f
NicoIsLost <contact@nicoislost.com>
2023-03-21 13:34:53 +0100

    File structure edits & client only

---
+++ src/main/java/com/nicoislost/TrendO.java
@@ -0,0 +1,12 @@
+package com.nicoislost;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TrendO implements ClientModInitializer {
+
+
+	@Override
+	public void onInitializeClient() {
+
+	}
+}
--- src/main/java/com/nicoislost/mixin/TrendOMixin.java
+++ src/main/java/com/nicoislost/mixin/TrendOMixin.java
@@ -7,9 +7,9 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(MinecraftServer.class)
-public class ExampleMixin {
+public class TrendOMixin {
 	@Inject(at = @At("HEAD"), method = "loadWorld")
 	private void init(CallbackInfo info) {
-		// This code is injected into the start of MinecraftServer.loadWorld()V
+		// This code is injected into the start of MinecraftServer.loadWorld()
 	}
 }
