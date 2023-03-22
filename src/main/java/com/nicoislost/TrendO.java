7d9233aea719f9b465c5181ba697a0ee20a647a5
Fej1Fun <guymoshegg007@gmail.com>
2023-03-22 13:56:02 +0200

    fixed bug

--- src/main/java/com/nicoislost/TrendO.java
+++ src/main/java/com/nicoislost/TrendO.java
@@ -3,13 +3,12 @@ package com.nicoislost;
 import com.nicoislost.util.ModRegistries;
 import net.fabricmc.api.ClientModInitializer;
 
-
 public class TrendO implements ClientModInitializer {
 
 
 	@Override
-
 	public void onInitializeClient() {
+
 		ModRegistries.registerModPackages();
 	}
 
--- src/main/java/com/nicoislost/mixin/FovChanger.java
+++
@@ -1,9 +0,0 @@
-package com.nicoislost.mixin;
-
-import net.minecraft.client.render.GameRenderer;
-import org.spongepowered.asm.mixin.Mixin;
-
-@Mixin(GameRenderer.class)
-public class FovChanger {
-
-}
