4498b97358ebe4614aff73c290401c79ec46f9a7
NicoIsLost <contact@nicoislost.com>
2023-03-25 21:56:18 +0100

    Added Key Press Order
    Added an algorithm to detect and save when the keys where pressed so that first pressed is prioritized.

--- src/main/java/com/nicoislost/ZoomO.java
+++ src/main/java/com/nicoislost/ZoomO.java
@@ -5,10 +5,16 @@ import com.nicoislost.owo.ZoomOConfig;
 import com.nicoislost.util.ModRegistries;
 import net.fabricmc.api.ClientModInitializer;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 
 public class ZoomO implements ClientModInitializer {
-
 	public static final ZoomOConfig CONFIG = ZoomOConfig.createAndLoad();
+
+	public static final Logger LOGGER = LoggerFactory.getLogger("modid");
+
+	private static final int[] zoomOrder = {0,0,0};
 
 	@Override
 	public void onInitializeClient() {
@@ -16,18 +22,87 @@ public class ZoomO implements ClientModInitializer {
 	}
 
 	public static boolean isZooming() {
-		if(KeyBinds.keyBinding1.isPressed() || KeyBinds.keyBinding2.isPressed() || KeyBinds.keyBinding3.isPressed()) {return true;}
-		return false;
+		return KeyBinds.keyBinding1.isPressed() || KeyBinds.keyBinding2.isPressed() || KeyBinds.keyBinding3.isPressed();
 	}
 
-	public static double zoomModifer() {
-		if(KeyBinds.keyBinding1.isPressed()) {
-			return (double)CONFIG.Zoom1();
-		} else if (KeyBinds.keyBinding2.isPressed()) {
-			return (double)CONFIG.Zoom2();
-		} else if (KeyBinds.keyBinding3.isPressed()) {
-			return (double)CONFIG.Zoom3();
+	public static int zoomModiferNum() {
+
+		LOGGER.info(String.valueOf(keyNum()));
+		LOGGER.info(String.valueOf(keyTotNum()));
+		if (keyTotNum() == 0) {
+			return 0;
+		} else if (keyTotNum() == 1) {
+
+			resetOrder();
+
+			if (KeyBinds.keyBinding1.isPressed()) {
+				zoomOrder[0] = 1;
+				return 1;
+			} else if (KeyBinds.keyBinding2.isPressed()) {
+				zoomOrder[0] = 2;
+				return 2;
+			} else if (KeyBinds.keyBinding3.isPressed()) {
+				zoomOrder[0] = 3;
+				return 3;
+			}
+		} else if (keyTotNum() == 2) {
+			if (zoomOrder[2] == 0) {
+				zoomOrder[1] = keyNum() / zoomOrder[0];
+				return zoomOrder[0];
+			} else {
+				for (int i = 0; i < 3; i++) {
+
+					boolean found = zoomOrder[i] == 6 / keyNum();
+
+					if (i == 2) {
+						zoomOrder[i] = 0;
+						break;
+					}
+
+					if (found) {
+						zoomOrder[i] = zoomOrder[i + 1];
+					}
+				}
+				return zoomOrder[0];
+			}
+		} else {
+			if (zoomOrder[2] == 0) {
+				zoomOrder[2] = 6 / (zoomOrder[0]*zoomOrder[1]);
+			}
+			return zoomOrder[0];
 		}
 		return 0;
+	}
+
+	public static double ZoomModifier(int keyNum) {
+		if (keyNum == 1) {
+			return CONFIG.Zoom1();
+		} else if (keyNum == 2) {
+			return CONFIG.Zoom2();
+		} else if (keyNum == 3) {
+			return CONFIG.Zoom3();
+		}
+		return 0;
+	}
+
+	public static int keyTotNum() {
+		int num = 0;
+		if (KeyBinds.keyBinding1.isPressed()) { ++num; }
+		if (KeyBinds.keyBinding2.isPressed()) { ++num; }
+		if (KeyBinds.keyBinding3.isPressed()) { ++num; }
+		return num;
+	}
+
+	public static int keyNum() {
+		int num = 1;
+		if (KeyBinds.keyBinding2.isPressed()) { num = num * 2; }
+		if (KeyBinds.keyBinding3.isPressed()) { num = num * 3; }
+		return num;
+	}
+
+	public static void resetOrder() {
+		zoomOrder[0] = 0;
+		zoomOrder[1] = 0;
+		zoomOrder[2] = 0;
 	}
 }
--- src/main/java/com/nicoislost/mixin/ZoomOMixin.java
+++ src/main/java/com/nicoislost/mixin/ZoomOMixin.java
@@ -1,15 +1,12 @@
 package com.nicoislost.mixin;
 
 import com.nicoislost.ZoomO;
-import com.nicoislost.inputs.KeyBinds;
 import net.minecraft.client.render.GameRenderer;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-import static com.nicoislost.ZoomO.CONFIG;
-import static com.nicoislost.ZoomO.zoomModifer;
 
 @Mixin(GameRenderer.class)
 public class ZoomOMixin {
@@ -17,7 +14,7 @@ public class ZoomOMixin {
 	private void getZoomLevel(CallbackInfoReturnable<Double> callbackInfo) {
 		if (ZoomO.isZooming()){
 			double fov = callbackInfo.getReturnValue();
-			callbackInfo.setReturnValue(fov * (1 - zoomModifer()/100));
+			callbackInfo.setReturnValue(fov * (1 - ZoomO.ZoomModifier(ZoomO.zoomModiferNum())/100));
 		}
 	}
 }
