bffd931986eadd01df6aa8b535d2f6ac749bcf84
Fej1Dev <guymoshegg007@gmail.com>
2023-03-27 22:49:04 +0300

    Updated config

--- src/main/java/com/nicoislost/mixin/ZoomOMixin.java
+++ src/main/java/com/nicoislost/mixin/ZoomOMixin.java
@@ -1,13 +1,11 @@
 package com.nicoislost.mixin;
 
-
 import com.nicoislost.ZoomO;
 import net.minecraft.client.render.GameRenderer;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
-
 
 @Mixin(GameRenderer.class)
 
--- src/main/java/com/nicoislost/owo/ZoomOConfigModel.java
+++ src/main/java/com/nicoislost/owo/ZoomOConfigModel.java
@@ -8,13 +8,13 @@ import io.wispforest.owo.config.annotation.RangeConstraint;
 @Config(wrapperName = "ZoomOConfig", name = "Zoom-O-Config")
 public class ZoomOConfigModel {
     @RangeConstraint(min = 1, max = 99)
-    public int Zoom1=30;
-    public boolean Zoom1SmoothCamera = false;
+    public int Zoom1=77;
+    public boolean Zoom1SmoothCamera = true;
     @RangeConstraint(min = 1, max = 99)
-    public int Zoom2=40;
+    public int Zoom2=70;
     public boolean Zoom2SmoothCamera = false;
     @RangeConstraint(min = 1, max = 99)
-    public int Zoom3=50;
+    public int Zoom3=60;
     public boolean Zoom3SmoothCamera = false;
 
 }
